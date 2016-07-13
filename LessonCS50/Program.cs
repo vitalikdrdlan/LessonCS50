aac3e114eb8de2b8dfa2c6ce5e0511f6019a1da4
Vitaly Dordolan <vitalikdrdlan@gmail.com>
2016-07-13 14:10:37 +0300

    Изменения кода для выбора функции (недоработано)

--- LessonCS50/Program.cs
+++ LessonCS50/Program.cs
@@ -14,12 +14,33 @@ namespace LessonCS50
 		public static void Main(string[] args)
 		{
 			LessonCS50_1 CS50 = new LessonCS50_1();
-			//CS50.fun_input_output();
-			//CS50.fun_loops();
-			
-			CS50.fun_switch(CS50.get_fun_switch());
+			/* Console.WriteLine("Выбирите какую функцию вы хотите запустить, нажмите от 1-4 и Ентер");
+			int Key;
+			switch(Console.ReadKey())
+			{
+				case ConsoleKey.NumPad1:
+					CS50.fun_input_output();
+				break;
+				case 50:
+					CS50.fun_loops();
+				break;
+				case 51:
+					CS50.fun_switch(CS50.get_fun_switch());
+				break;
+				case 52:
+					CS50.F2C(CS50.getF2C());
+				break;
+				case 13:
+					CS50.F2C(CS50.getF2C());
+				break;
+				default:
+					{
+						Console.WriteLine("Это число {0} НЕ в пределах существующих операции",Key);
+					}
+					break;
+			}
+			*/
 			CS50.F2C(CS50.getF2C());
-			
 			Console.ReadKey(true);
 		}	
 	}
