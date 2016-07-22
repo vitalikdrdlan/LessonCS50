aef8dec1cdf819bf3ef7078650630f60806f090c
Vitaly <vitalikdrdlan@gmail.com>
2016-07-22 11:12:42 +0300

    Добавление консольных команд, фи функция вывода вертикального текста

--- LessonCS50_linux/LessonCS50_2.cs
+++ LessonCS50_linux/LessonCS50_2.cs
@@ -4,17 +4,29 @@ namespace LessonCS50_2
 {
 	public class LCS50_2
 	{
-		/// <summary>
-		/// Конструктор класса LessonCS50_1
-		/// </summary>
+		// <summary>
+		// Конструктор класса LessonCS50_1
+		// </summary>
 		public LCS50_2 ()
 		{
 		}
-		public void Cript(string str)
+		// <summary>
+		// Статический Метод выводящий строку вертикально
+		// </summary>
+		public static void vstr(string str)
 		{
 			if(str != null)
 			for(int i=0;i<str.Length;i++)
 			Console.WriteLine ("{0}",str[i]);
+		}
+		// <summary>
+		// Метод кодирования информации алгоритмом RSA, в данном случае строки
+		// </summary>
+		public static void RSA(string str)
+		{
+			if(str != null)
+				for(int i=0;i<str.Length;i++)
+					Console.WriteLine ("{0}",str[i]);
 		}
 	}
 }
